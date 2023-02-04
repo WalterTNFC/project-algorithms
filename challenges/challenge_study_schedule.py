7440c9948f8e7e46afda1fcd866e086ea98d5ac4
WalterTNFC <walter.tad@hotmail.com>
2023-02-04 19:52:35 -0300

    feat: Verify req 1- students schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count_students_online = 0
+
+    for start, end in permanence_period:
+        if (type(start) is not int or type(end) is not int):
+            return None
+        if start <= target_time <= end:
+            count_students_online += 1
+
+    return count_students_online
