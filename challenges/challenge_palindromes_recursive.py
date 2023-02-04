dfb0d577e60c96e8fca9ddb9137f805015d799a7
WalterTNFC <walter.tad@hotmail.com>
2023-02-04 20:43:33 -0300

    feat: Verify req-3 palindromes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,18 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) is None:
+        return False
+
+    # Falso se o ultimo e primeiro
+    # caracteres forem diferentes
+    if word[low_index] != word[high_index]:
+        return False
+
+    if low_index == high_index or (low_index - high_index) == 1:
+        return True
+
+    next_index = low_index + 1
+    past_index = high_index - 1
+
+    verify = is_palindrome_recursive(word, next_index, past_index)
+
+    return verify
