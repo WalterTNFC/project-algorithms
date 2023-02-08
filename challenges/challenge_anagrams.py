630a86b2cd02fb868eaff547e682307115717a77
WalterTNFC <walter.tad@hotmail.com>
2023-02-07 22:01:56 -0300

    feat: init req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,8 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # """Faça o código aqui."""
+    # raise NotImplementedError
+    first_list = [*first_string.lower()]
+    second_list = [*second_string.lower()]
+    print(first_list)
+    print(second_list)
+
