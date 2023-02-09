b7a9a9b79f534580d00a6403b377a73fc9ff6548
WalterTNFC <walter.tad@hotmail.com>
2023-02-09 17:17:01 -0300

    deat: Verify req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -4,8 +4,11 @@ def find_duplicate(nums):
     if len(nums) <= 1:
         return False
 
-    # if isinstance(nums[0], str) or nums[0] < 0:
-    #     return False
+    if isinstance(nums[0], str) or nums[0] < 0:
+        return False
+    # Ideia de usar sort sugerida na monitoria
+    #  O avaliador estava quebrando por conta da complexbilidade
+    # sort_nums = sorted(nums)
 
     # for i, x in enumerate(nums):
     #     if x in nums[:i]:
