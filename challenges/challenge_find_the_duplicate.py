19ba82bd7f3f52b1f41df6acdbf547f339ddac91
WalterTNFC <walter.tad@hotmail.com>
2023-02-08 23:54:32 -0300

    feat: Verify req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1:
+        return False
+
+    if isinstance(nums[0], str) or nums[0] < 0:
+        return False
+
+    for i, x in enumerate(nums):
+        if x in nums[:i]:
+            return x
+
+    return False
