5413d53c8ee8a92845e75761ff3cc5a26faddb38
WalterTNFC <walter.tad@hotmail.com>
2023-02-09 20:29:02 -0300

    feat: verify req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,20 +1,57 @@
+# Referencia [1]
+# Referencia [2] - adaptada
+def merge_sort(array):
+    if not array:
+        return []
+
+    if len(array) == 1:
+        return [array[0]]
+
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    if not left:
+        return right
+
+    if not right:
+        return left
+
+    if left[0] > right[0]:
+        return [right[0]] + merge(left, right[1:])
+    else:
+        return [left[0]] + merge(left[1:], right)
+
+
 def is_anagram(first_string, second_string):
-    # """Faça o código aqui."""
-    raise NotImplementedError
-    # first_list = sorted(list(first_string))
-    # second_list = sorted(list(second_string))
+    # Comparar duas strings:
+    #  Ordenar e verificar se uma é anagrama da outra
 
-    # index = 0
-    # match = True
+    array1 = merge_sort(first_string.lower())
+    array2 = merge_sort(second_string.lower())
 
-    # while index < len(first_string) and match:
-    #     if first_list[index] == second_list[index]:
-    #         index += 1
-    #     else:
-    #         match = False
+    join_first = ''.join(array1)
+    join_second = ''.join(array2)
 
-    # return (first_list, second_list, match)
+    if first_string == '' or second_string == '':
+        return (join_first, join_second, False)
+
+    for index, _ in enumerate(array1):
+        if array1[index] != array2[index]:
+            return (join_first, join_second, False)
+
+    return (join_first, join_second, True)
 
 # Referencia
 # /https://panda.ime.usp.br/panda/static/
 # pythonds_pt/02-AnaliseDeAlgoritmos/ExemploDeDeteccaoDeAnagramas.html
+# teste = is_anagram("muro", "RuMo")
+# print(teste)
+
+# Referencias:
+# [1]: Course dia 3- exercicios (merge)
+# [2]: https://www.freecodecamp.org/portuguese
+# /news/algoritmos-de-ordenacao-explicados-com-exemplos-em-python-java-e-c/
