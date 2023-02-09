2d3f35f3dba9c4821599896641b637e30edf8cbf
WalterTNFC <walter.tad@hotmail.com>
2023-02-09 00:22:45 -0300

    feat: Verify req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,18 @@
 def is_anagram(first_string, second_string):
-    # """Faça o código aqui."""
-    # raise NotImplementedError
-    first_list = [*first_string.lower()]
-    second_list = [*second_string.lower()]
-    print(first_list)
-    print(second_list)
+    first_list = sorted(list(first_string))
+    second_list = sorted(list(second_string))
+
+    index = 0
+    match = True
+
+    while index < len(first_string) and match:
+        if first_list[index] == second_list[index]:
+            index += 1
+        else:
+            match = False
+
+    return (first_list, second_list, match)
+
+# Referencia
+# /https://panda.ime.usp.br/panda/static/
+# pythonds_pt/02-AnaliseDeAlgoritmos/ExemploDeDeteccaoDeAnagramas.html
