1786c4155578fbd5b299d2d8e404d82a9d040ddd
WalterTNFC <walter.tad@hotmail.com>
2023-02-09 00:28:07 -0300

    feat: Verify req test

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,11 @@
 def is_palindrome_iterative(word):
-    if len(word) == 0:
-        return False
+    # if len(word) == 0:
+    #     return False
 
-    for i in range(0, int(len(word)/2)):
-        if word[i] != word[len(word)-i-1]:
-            return False
-    return True
+    # for i in range(0, int(len(word)/2)):
+    #     if word[i] != word[len(word)-i-1]:
+    #         return False
+    # return True
 
 # Referencia:
 # https://acervolima.com/
