aab3a8031f672b830f9c028c28f225421637db08
WalterTNFC <walter.tad@hotmail.com>
2023-02-09 13:18:46 -0300

    feat: verify test

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,12 +1,14 @@
 def find_duplicate(nums):
-    if len(nums) <= 1:
-        return False
+    # """Faça o código aqui."""
+    raise NotImplementedError
+    # if len(nums) <= 1:
+    #     return False
 
-    if isinstance(nums[0], str) or nums[0] < 0:
-        return False
+    # if isinstance(nums[0], str) or nums[0] < 0:
+    #     return False
 
-    for i, x in enumerate(nums):
-        if x in nums[:i]:
-            return x
+    # for i, x in enumerate(nums):
+    #     if x in nums[:i]:
+    #         return x
 
-    return False
+    # return False
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,13 +1,13 @@
 def is_palindrome_iterative(word):
     # """Faça o código aqui."""
-    raise NotImplementedError
-    # if len(word) == 0:
-    #     return False
+    # raise NotImplementedError
+    if len(word) == 0:
+        return False
 
-    # for i in range(0, int(len(word)/2)):
-    #     if word[i] != word[len(word)-i-1]:
-    #         return False
-    # return True
+    for i in range(0, int(len(word)/2)):
+        if word[i] != word[len(word)-i-1]:
+            return False
+    return True
 
 # Referencia:
 # https://acervolima.com/
